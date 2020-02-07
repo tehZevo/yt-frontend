17ce12d31a4fe89a57bc2f3d57e112cb280b5dea
tehZevo <tehzevo@users.noreply.github.com>
2020-02-07 18:57:07 -0500

    Work on channel scraping

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 A UI that autoplays videos from channels you specify.
 
-## Notes:
-* Currently relies on a CORS proxy to scrape YouTube playlist pages (thanks [cors-anywhere](https://github.com/Rob--W/cors-anywhere)
+## Notes
+* Currently relies on a CORS proxy to scrape YouTube playlist pages (thanks [cors-anywhere](https://github.com/Rob--W/cors-anywhere))
+
+## TODO
+* Ability to add channels
+* Automatically scrape channels on launch + periodic scrapes while running
+* Save/load button
+* Channel filter (checkboxes/toggle all)
--- app.js
+++ app.js
@@ -15,6 +15,52 @@ function playlistURL(uid)
   return `${corsProxy}https://www.youtube.com/playlist?list=UU${uid}&disable_polymer=true`;
 }
 
+function channelURL(username)
+{
+  return `${corsProxy}https://www.youtube.com/user/${username}/?disable_polymer=true`;
+}
+
+function channelIdURL(id)
+{
+  return `${corsProxy}https://www.youtube.com/channel/${id}/?disable_polymer=true`;
+}
+
+async function getChannelInfo(id)
+{
+  //channel-header-profile-image
+  var body = await fetch(channelIdURL(id), {
+    headers: {
+      "X-Requested-With": "fetch"
+    },
+  });
+
+  body = await body.text();
+
+  var s = $(body);
+  var channelIcon = s.find(".channel-header-profile-image").attr("src")
+  var banner = s.find(".hd-banner-image").css('background-image') //TODO
+  //banner = banner.replace('url(','').replace(')','').replace(/\"/gi, "");
+  console.log({channelIcon, banner})
+  return {channelIcon, banner}
+}
+
+async function getIdOfUser(user)
+{
+  //fetch https://www.youtube.com/user/<user>/?disable_polymer=1
+  //<meta itemprop="channelId" content="UCXuqSBlHAE6Xw-yeJA0Tunw">
+  var body = await fetch(channelURL(user), {
+    headers: {
+      "X-Requested-With": "fetch"
+    },
+  });
+
+  body = await body.text();
+
+  //h̶̿ͧ̀e̿͑ͨ̈ͤ́ͧ ̨ͤ̎̾̃ͤç̎́͊ͫo̾͆̍̇̈́̓m͐̏̈ͤͪ͋̀e̓̌͂̓ͤ̀ŝ͠
+  var match = body.match(/<meta\s+itemprop="channelId"\s+content="(.*)"/)
+  return match[1];
+}
+
 async function getVideosOfChannel(uid)
 {
   if(uid.startsWith("UU") || uid.startsWith("UC"))
@@ -39,10 +85,9 @@ async function getVideosOfChannel(uid)
     var titleLink = e.find(".pl-video-title-link");
     var id = extractVideoID(titleLink.attr("href"));
     var title = titleLink.text().trim();
-    var thumb = `https://img.youtube.com/vi/${id}/0.jpg`;
     var length = e.find(".timestamp > span").text();
 
-    return {id, title, thumb, length};
+    return {id, title, length};
   }).toArray();
 
   return videos;
